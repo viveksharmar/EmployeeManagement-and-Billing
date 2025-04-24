cfeae6864daab66dd115f150004c5c189a6804ae
viveksharmar <146196605+viveksharmar@users.noreply.github.com>
2025-04-24 21:41:06 +0530

    EmployeeManagement-and-Billing

---
+++ EmployeePortal/Repository/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using EmployeePortal.Data;
+using EmployeePortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Repository
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext db;
+        public EmployeeRepository(AppDbContext dbContext)
+        {
+            this.db = dbContext;
+        }
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await db.Employees.ToListAsync();
+        }
+        public async Task SaveEmployee(Employee emp)
+        {
+            await db.Employees.AddAsync(emp);
+            await db.SaveChangesAsync();
+        }
+        public async Task updateEmployee(int id, Employee obj)
+        {
+            var employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee Not Found");
+            }
+            employee.Name = obj.Name;
+            employee.Email = obj.Email;
+            employee.Mobile = obj.Mobile;
+            employee.Age = obj.Age;
+            employee.Salary = obj.Salary;
+            employee.Status = obj.Status;
+
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            var employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee Not Found");
+            }
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+        }
+    }
+}
